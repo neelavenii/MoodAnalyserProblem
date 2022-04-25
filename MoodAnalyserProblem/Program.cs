62fb872b79d5e7117f81bf10bd9586ede5d5827f
eswar <neelaveni.eswar04@gmail.com>
2022-04-25 10:58:25 +0530

    [Eswar]Added Mood Analyser Problem

---
+++ MoodAnalyserProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Problem");
+            Console.ReadLine();
+        }
+    }
+}
